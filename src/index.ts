9190e9833c51d8765e252e223c46b8e30b56a398
Andrey Nadosha <andrey.nadosha@gmail.com>
2022-06-15 23:25:13 +0300

    Now hraphql setup and connected to FE

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import mongoose from "mongoose";
 import schema from "./schema";
-// import mock from "../FAKE_DATA.json";
-// import { Steps } from "./startup-progress/model";
+ import mock from "../FAKE_DATA.json";
+ import { Posts } from "./services/example/model";
 
 const startServer = async () => {
   const app = express();
@@ -21,6 +21,14 @@ const startServer = async () => {
 
   await mongoose.connect("mongodb://localhost:27017/startup");
   console.log("Mongoose connected ...");
+
+  Posts.find().count((err, count) => {
+    if(!count) {
+      Posts.insertMany(mock);
+      console.log("Data successfully mocked!");
+    }
+  });
+
 
   app.listen(3030, () => console.log("Server is running on port 3030"));
 };
